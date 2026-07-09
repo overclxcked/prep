38cf6e913ae31aa9ee75fe2f8cd8897ee9cb029f
khalid <khalidelpool@gmail.com>
2026-07-09 08:50:28 +0100

    finished vect2

--- mine/level0/vect2/vect2.cpp
+++ mine/level0/vect2/vect2.cpp
@@ -4,7 +4,7 @@ vect2::vect2(): a(0), b(0){}
 
 vect2::vect2(int c, int d): a(c), b(d){}
 
-const vect2& vect2::operator=(const vect2& other)
+vect2& vect2::operator=(const vect2& other)
 {
     if (this != &other)
     {
@@ -14,12 +14,9 @@ const vect2& vect2::operator=(const vect2& other)
     return *this;
 }
 
-vect2::vect2(const vect2& other) { operator=(other); }
+vect2::vect2(const vect2& other): a(other.a), b(other.b) {}
 
 vect2::~vect2(){}
-
-int vect2::get_a() const { return a; };
-int vect2::get_b() const { return b; };
 
 int& vect2::operator[](int idx) { if (idx == 0) return a; return b; }
 
@@ -33,21 +30,11 @@ vect2 vect2::operator++(int) { vect2 tmp(*this); a++; b++; return tmp; }
 
 vect2 vect2::operator--(int) { vect2 tmp(*this); a--; b--; return tmp; }
 
-vect2& vect2::operator+=(const int& value) { a += value; b += value; return *this; }
-
-vect2& vect2::operator-=(const int& value) { a -= value; b -= value; return *this; }
-
 vect2& vect2::operator*=(const int& value) { a *= value; b *= value; return *this; }
 
 vect2& vect2::operator+=(const vect2& other) { a += other.a; b += other.b; return *this; }
 
 vect2& vect2::operator-=(const vect2& other) { a -= other.a; b -= other.b; return *this; }
-
-vect2& vect2::operator*=(const vect2& other) { a *= other.a; b *= other.b; return *this; }
-
-vect2& vect2::operator+(const int& value) { a += value; b += value; return *this; }
-
-vect2& vect2::operator-(const int& value) { a -= value; b -= value; return *this; }
 
 vect2 vect2::operator*(const int& value) const { vect2 v(a * value, b * value); return v; }
 
@@ -57,14 +44,12 @@ vect2 vect2::operator-(const vect2& other) const { vect2 v(a - other.a, b - othe
 
 vect2 vect2::operator-() const { vect2 v(-a, -b); return v; }
 
-vect2& vect2::operator*(const vect2& other) { a *= other.a; b *= other.b; return *this; }
+bool vect2::operator==(const vect2& other) const { return (a == other.a && b == other.b); }
 
-bool vect2::operator==(const vect2& other) { return (a == other.a && b == other.b); }
+bool vect2::operator!=(const vect2& other) const { return !(a == other.a && b == other.b); }
 
-bool vect2::operator!=(const vect2& other) { return !(a == other.a && b == other.b); }
-
-vect2 operator*(int value, const vect2& obj) { return vect2(); }
+vect2 operator*(int value, const vect2& obj) { return obj * value; }
 std::ostream& operator<<(std::ostream& ostr, const vect2& obj)
 {
-    return ostr << "{" << obj.get_a() << ", " << obj.get_b() << "}" << std::endl;
+    return ostr << "{" << obj[0] << ", " << obj[1] << "}";
 }
--- mine/level0/vect2/vect2.hpp
+++ mine/level0/vect2/vect2.hpp
@@ -10,19 +10,14 @@ class vect2 {
         vect2(int a, int b);
         vect2(const vect2& other);
         ~vect2();
-
-        int get_a() const ;
-        int get_b() const ;
         
-        const vect2& operator=(const vect2& other);
+        vect2& operator=(const vect2& other);
         int& operator[](int idx);
         const int& operator[](int idx) const;
         vect2& operator++();
         vect2& operator--();
         vect2 operator++(int);
         vect2 operator--(int);
-        vect2& operator+=(const int& value);
-        vect2& operator-=(const int& value);
         vect2& operator*=(const int& value);
         vect2& operator+=(const vect2& other);
         vect2& operator-=(const vect2& other);
@@ -33,14 +28,8 @@ class vect2 {
         vect2 operator-(const vect2& other) const;
         vect2 operator*(const int& value) const;
 
-        vect2& operator+(const int& value);
-        vect2& operator-(const int& value);
-        vect2& operator*(const vect2& other);
-        
-        vect2& operator*=(const vect2& other);
-        
-        bool operator==(const vect2& other);
-        bool operator!=(const vect2& other);
+        bool operator==(const vect2& other) const;
+        bool operator!=(const vect2& other) const;
 };
 
 std::ostream& operator<<(std::ostream& ostr, const vect2& obj);
